dfabbd966e5fba437311fa1df21bec3212ee366a
Derick12345678 <4realderek@gmail.com>
2025-02-13 00:51:09 -0500

    Added jsx files for the different components Added basic css for the files

---
+++ front_end/src/components/TopLeftButtons.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TopLeftButtons = () => {
+  return (
+    <div className="top-left-buttons">
+      <button>Private Messages</button>
+      <button>Group Messages</button>
+    </div>
+  );
+};
+
+export default TopLeftButtons;
