bb6383b74adb8eddfaf83481b2f8d969e2b02210
Derick12345678 <148519593+Derick12345678@users.noreply.github.com>
2025-02-15 13:44:17 -0500

    Derek: Worked on the front end UI -worked on a UI for viewing available channels -created new jsx files for new components -added images that will be used as icons

--- front_end/src/components/UserSidebar.jsx
+++ front_end/src/components/UserSidebar.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 
+const users = [
+  { name: "Current User", role: "Role" },
+  { name: "Other User", role: "Role" },
+  { name: "Other User", role: "Role" },
+  { name: "Other User", role: "Role" },
+];
+//I'm assuming this will be dynamically added ^^^
+
 const UserSidebar = () => {
   return (
     <div className="user-sidebar">
-      <h3>Users</h3>
-      <div>Current User</div>
-      <div>Other User</div>
-      <div>Other User</div>
-      <div>Other User</div>
+      {users.map((user, index) => (
+        <div key={index} className="user-card">
+          <div className="avatar">
+            <img src="profile-user.png" alt="User Avatar" />
+          </div>
+          <div className="user-info">
+            <div className="user-name">{user.name}</div>
+            <div className="user-role">{user.role}</div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
