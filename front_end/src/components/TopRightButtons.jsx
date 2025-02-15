cae473e409efafdff94147ce8f72ba97d20d54ef
Gavin Chock-Chiong <gavin.chockchiong@gmail.com>
2025-02-15 16:36:10 -0500

    fixed the look of the buttons on the top right

--- front_end/src/components/TopRightButtons.jsx
+++ front_end/src/components/TopRightButtons.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import trashIcon from "../../public/trash-bin.png"
+import settingsIcon from "../../public/settings-icon.png"
 const TopRightButtons = () => {
   return (
     <div className="top-right-buttons">
       <button>+</button>
-      <button>x</button>
-      <button>#</button>
+      <button ><img src = {trashIcon} alt = "Trash-bin icon" className="trash-icon"/></button>
+      <button><img src = {settingsIcon} alt = "Trash-bin icon" className = "trash-icon"></img></button>
     </div>
   );
 };
