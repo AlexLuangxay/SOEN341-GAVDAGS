118c7cff894cd372b84c614545bf846117eeac33
Derick12345678 <148519593+Derick12345678@users.noreply.github.com>
2025-02-26 15:43:33 -0500

    Added PrivateMessageView.jsx and GroupMessageView.jsx and integrated it with main.jsx - Updated App.jsx to include GroupChatName and reorganized UI components - Installed and configured react-router-dom for navigation - Fixed import issues and ensured proper rendering of components - Improved button structure in TopLeftButtons for navigation

--- front_end/src/pages/App.jsx
+++ front_end/src/pages/App.jsx
@@ -1,16 +1,14 @@
-//We need to change this to add routing
-
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import "./App.css";
 import ChatWindow from "../components/ChatWindow";
-import Groups from "../components/Groups";
+import People from "../components/People";
 import Channels from "../components/Channels";
 import MessageBar from "../components/MessageBar";
 import UserSidebar from "../components/UserSidebar";
 import TopLeftButtons from "../components/TopLeftButtons";
 import TopRightButtons from "../components/TopRightButtons";
-import GroupChatName from "../components/GroupChatName";
+import ChatName from "../components/ChatName";
 
 const socket = io('http://localhost:5000');
 
@@ -42,13 +40,12 @@ function App() {
     <div className="App">
       <header className="top-bar">
         <TopLeftButtons />
-        <GroupChatName />
+        <ChatName />
         <TopRightButtons />
       </header>
       <div className="main-container">
         <aside className="left-sidebar">
-          <Groups socket={socket}/>
-          <Channels />
+          <People socket={socket}/>
         </aside>
         <main className="chat-container">
           <ChatWindow messages={messages}/>
