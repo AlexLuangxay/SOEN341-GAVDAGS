0e64ec5eeaadfee049db3fd052063c3248900ad6
Gur <gurlal5231@gmail.com>
2025-03-19 13:25:51 -0400

    Optimized removing self from DM's #13

--- back_end/main.py
+++ back_end/main.py
@@ -17,8 +17,7 @@ socketIO = SocketIO(app, cors_allowed_origins="*")
 @app.route('/users', methods=['GET'])
 def fetch_users():
     users = get_all_users() 
-    current_user = session.get('user')  
-    users = [user for user in users if user["name"] != current_user]
+    users = [user for user in users if user["name"] != session.get('user')]
     return jsonify(users), 200 
 
 @app.route("/signup", methods=["POST"])
