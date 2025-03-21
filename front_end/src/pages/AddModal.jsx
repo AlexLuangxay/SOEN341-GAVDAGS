df43d9c4fa4c21bb3b9801441cb0cd9ef9c5aa46
simonbern <151587638+simonbern@users.noreply.github.com>
2025-03-21 15:07:01 -0400

    Modified the add modal (#56)

--- front_end/src/pages/AddModal.jsx
+++ front_end/src/pages/AddModal.jsx
@@ -1,23 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
-const AddModal = ({ isOpen, onClose }) => {
+const AddModal = ({ isOpen, onClose, onAddChannel }) => {
+  const [newChannel, setNewChannel] = useState("");
 
   if (!isOpen) {
     return null;
   }
 
+  const handleAdd = () => {
+    if (newChannel.trim() !== "") {
+      onAddChannel(newChannel);
+      setNewChannel("");
+      onClose();
+    }
+  };
+
   return (
     <div className="Settingsmodal">
       <div className="Settingsmodal-content">
         <span className="Settingsclose" onClick={onClose}>&times;</span>
-        <h2>Add</h2>
-        <div>
-          <label>
-            Gur Lal
-          </label>
+        <h2>Add Channel</h2>
+        <div style={{ marginTop: "10px" }}>
+          <input 
+            type="text" 
+            value={newChannel} 
+            onChange={(e) => setNewChannel(e.target.value)} 
+            placeholder="Enter channel name"
+            style={{ display: "block", marginBottom: "10px", width: "100%" }}
+          />
+          <button onClick={handleAdd}>Add</button>
         </div>
       </div>
     </div>
   );
 };
+
 export default AddModal;
