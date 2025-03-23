87f432b655ccd90e7b18e031674b3786cc1bf234
Alex Luangxay <alexluangxay@gmail.com>
2025-03-22 20:39:57 -0400

    Create GroupMembers.jsx file #17

--- front_end/src/components/GroupeSidebar.jsx
+++ front_end/src/components/GroupeSidebar.jsx
@@ -1,22 +1,14 @@
 import React, { useEffect, useState } from "react";
 
 const GroupeSidebar = ({ guildMembers }) => {
-    const [members, setMembers] = useState([]);
 
-    useEffect(() => {
-        if (guildMembers) {
-            setMembers(guildMembers);
-        }
-    }, [guildMembers]);
-
+    // get guild members
+    
+    
     return (
         <div className="sidebar">
             <h2>Guild Members</h2>
-            <ul>
-                {members.map((member, index) => (
-                    <li key={index}>{member}</li>
-                ))}
-            </ul>
+  
         </div>
     );
 };
