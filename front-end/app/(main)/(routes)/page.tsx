02d831766067af9dd7898fc9d04292228c6e8da4
Alex Luangxay <alexluangxay@gmail.com>
2025-03-28 18:47:36 -0400

    Authentification upgraded #93

--- front-end/app/(main)/(routes)/page.tsx
+++ front-end/app/(main)/(routes)/page.tsx
@@ -1,11 +1,12 @@
 import {Button} from "@/components/ui/button";
 import { DESTRUCTION } from "dns";
 import { cn } from "@/lib/utils";
+import {UserButton} from "@clerk/nextjs";
 const state = true;
 export default function Home() {
   return (
    <div  >
-  This is a protected route
+    <UserButton/>
     </div>
   );
 }
