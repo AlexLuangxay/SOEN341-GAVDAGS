67ecc08b5fe045ede30dadf248d2aa79c68bea6d
valerie-nguyen <val.nguyen08@gmail.com>
2025-03-28 11:58:02 -0400

    Modified UserSidebarDM to fetch status of selectedUser (#66)
    - Bug to fix: fetch status of current user

--- front_end/src/components/UserSidebarDM.jsx
+++ front_end/src/components/UserSidebarDM.jsx
@@ -1,14 +1,26 @@
 import React, { useEffect, useState } from "react";
 
-const UserSidebarDM = ({ selectedUser, status }) => {
+const UserSidebarDM = ({ selectedUser }) => {
   const [currentUser, setCurrentUser] = useState(null);
-
+  const [currentUserStatus, setCurrentUserStatus] = useState("Online");
+  const [selectedUserStatus, setSelectedUserStatus] = useState("Offline");
+  
   useEffect(() => {
     fetch("http://localhost:5001/current_user", { credentials: "include" }) 
       .then((response) => response.json())
       .then((data) => setCurrentUser(data))
       .catch((error) => console.error("Error fetching user:", error));
   }, []);
+
+  // Fetch selected user's status
+  useEffect(() => {
+    if (selectedUser) {
+      fetch(`http://localhost:5001/get_user_status?username=${selectedUser}`)
+        .then((response) => response.json())
+        .then((statusData) => setSelectedUserStatus(statusData.status ? "Online" : "Offline"))
+        .catch((error) => console.error("Error fetching selected user status:", error));
+    }
+  }, [selectedUser]);
 
   return (
     <div className="user-sidebar">
@@ -19,8 +31,8 @@ const UserSidebarDM = ({ selectedUser, status }) => {
         <div className="user-info">
           <div className="user-name">{currentUser ? currentUser : "Loading..."}</div>
           <div className={"user-status"}>
-              <span className={`status-circle ${status}`}></span>
-              {status}
+              <span className={`status-circle ${currentUserStatus}`}></span>
+              {currentUserStatus}
               </div>
         </div>
       </div>
@@ -32,6 +44,10 @@ const UserSidebarDM = ({ selectedUser, status }) => {
           </div>
           <div className="user-info">
             <div className="user-name">{selectedUser}</div>
+            <div className={"user-status"}>
+               <span className={`status-circle ${selectedUserStatus}`}></span>
+               {selectedUserStatus}
+             </div>
           </div>
         </div>
       ) : (
