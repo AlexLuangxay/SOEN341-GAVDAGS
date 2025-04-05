3d050aa67b1f1c5ee77f1b5dfc64194965293f8a
Alex Luauangxay <alexluangxay@gmail.com>
2025-04-05 18:28:31 -0400

    update testCase file #64

--- front_end/reactTestCases.test.js
+++ front_end/reactTestCases.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import AddModal from './src/pages/AddModal.jsx';
+import React from 'react'; 
 
 test('AddModal renders correctly', () => {
   render(<AddModal isOpen={true} onClose={() => {}} onAddChannel={() => {}} />);
