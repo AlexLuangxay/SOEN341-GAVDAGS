fb40218c7b5f6039a3593235083fdf669cd4538a
Alex Luauangxay <alexluangxay@gmail.com>
2025-04-05 18:52:18 -0400

    update files to pass react test #64

--- front_end/jest.config.cjs
+++ front_end/jest.config.cjs
@@ -1,10 +1,8 @@
 module.exports = {
-  setupFiles: ['<rootDir>/jest.setup.cjs'],
-  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.cjs'], // ✅ only this
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(gif|ttf|eot|svg|png|jpg)$': '<rootDir>/fileMock.js'
   }
 };
-
--- front_end/jest.setup.cjs
+++ front_end/jest.setup.cjs
@@ -1,3 +1,6 @@
+require('@testing-library/jest-dom');
+console.log('OKAY jest.setup.cjs loaded');
+
 const { TextEncoder, TextDecoder } = require('util');
 
 global.TextEncoder = TextEncoder;
